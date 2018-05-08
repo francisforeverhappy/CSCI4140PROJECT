a8be2c9f207fa2c6b400598a64dd8b4e6d9189d8
rzli6 <1155076990@link.cuhk.edu.hk>
2018-05-08 16:45:22 +0800

    modular backend

---
+++ routes/protected.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+    router = express.Router(),
+    spawn = require('child_process').spawn;
+
+const Course = require('../models/course'),
+    Section = require('../models/section'),
+    Comment = require('../models/comment'),
+    middleware = require('../middleware'),
+    support = require('../support/js/support');
+
+// login needed
+router.get('/getWait', middleware.checkLogin, middleware.asyncMiddleware(async (req, res) => {
+    console.log('get /getWait');
+    // TBI
+}));
+
+router.get('/import', middleware.checkLogin, middleware.asyncMiddleware(async (req, res) => {
+    console.log('get /import');
+    let sid = req.session.sid,
+        pwd = support.decrypt(sid, req.session.pwd);
+    let pythonProcess = spawn('python', ['support/py/import.py', sid, pwd]);
+    pythonProcess.stdout.on('data', async (data) => {
+        let courseNumbers = data.toString().trim().split(',').map(Number); 
+        console.log(courseNumbers);
+        let sections = await Section.find({courseNumber: {$in: courseNumbers}});
+        sections.map(async section => {
+            section.course = await Course.findById(section.courseInfo);
+        });
+        res.send({sid: sid, sections: sections})
+    });
+    // TBI
+}));
