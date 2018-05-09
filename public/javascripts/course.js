c1296a489e03857342beaa8d9288ff81d22525ba
francisforeverhappy <francis0322happy@gmail.com>
2018-05-09 17:34:37 +0800

    unfinished meeting info

--- public/javascripts/course.js
+++ public/javascripts/course.js
@@ -30,23 +30,3 @@ $(".comments-sort-list").children().on('click', function (e) {
     $.get($(this).attr('title'),sortcomments);
 });
 
-// meeting time table
-function altRows(id) {
-    if (document.getElementsByTagName) {
-
-        var table = document.getElementById(id);
-        var rows = table.getElementsByTagName("tr");
-
-        for (i = 0; i < rows.length; i++) {
-            if (i % 2 == 0) {
-                rows[i].className = "evenrowcolor";
-            } else {
-                rows[i].className = "oddrowcolor";
-            }
-        }
-    }
-}
-
-window.onload = function () {
-    altRows('alternatecolor');
-}
