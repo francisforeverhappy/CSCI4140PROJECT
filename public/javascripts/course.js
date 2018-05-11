e74f653da45d3464441b674139d98d73060ef4f5
francisforeverhappy <francis0322happy@gmail.com>
2018-05-11 14:15:20 +0800

    for new schema

--- public/javascripts/course.js
+++ public/javascripts/course.js
@@ -125,3 +125,28 @@ $('#Delete').on("click", function () {
         }
     });
 });
+
+//like and unlike
+$(function () {
+    $("#praise").click(function () {
+        var praise_img = $("#praise-img");
+        var text_box = $("#add-num");
+        var praise_txt = $("#praise-txt");
+        var num = parseInt(praise_txt.text());
+        if (praise_img.attr("src") == ("/img/like.png")) {
+            $(this).html("<img src='/img/unlike.png' id='praise-img' class='animation' />");
+            praise_txt.removeClass("hover");
+            text_box.show().html("<em class='add-animation'>-1</em>");
+            $(".add-animation").removeClass("hover");
+            num -= 1;
+            praise_txt.text(num);
+        } else {
+            $(this).html("<img src='/img/like.png' id='praise-img' class='animation' />");
+            praise_txt.addClass("hover");
+            text_box.show().html("<em class='add-animation'>+1</em>");
+            $(".add-animation").addClass("hover");
+            num += 1;
+            praise_txt.text(num);
+        }
+    });
+});
