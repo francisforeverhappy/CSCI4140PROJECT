7651670580615d84df38405bf896ee690a90929f
rzli6 <1155076990@link.cuhk.edu.hk>
2018-05-11 20:34:37 +0800

    recommendation

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,37 @@ const Course = require('../models/course'),
     middleware = require('../middleware'),
     support = require('../support/js/support');
 
-router.get('/', (req, res) => {
-    let isLoggedIn = false;
-    res.render('index', {sid: req.session.sid});
+router.get('/', (req, res) => {    
+    return res.render('index', {sid: req.session.sid});
 });
 
+router.get('/recommend', middleware.asyncMiddleware(async (req, res) => {
+    const courseMessage = 'courseCode courseName classDetails.units sectionCode';
+    if ('sid' in req.session) {
+        let sid = req.session.sid,
+            pwd = support.decrypt(sid, req.session.pwd);
+        
+        let pythonProcess = spawn('python', ['support/py/recommend.py', sid, pwd]);
+        pythonProcess.stdout.on('data', async (data) => {
+            courseCodes = data.toString().trim().split(',');
+            let courses = courseCodes.map(async (courseCode) => {
+                course = await Course.findOne({courseCode: courseCode}, courseMessage).lean();
+                return course;
+            });
+            courses = await Promise.all(courses);
+            return res.send({sid: req.session.sid, courses: courses});
+        });
+
+    } else {
+        let courses = await Course.find({}, courseMessage, {sort: {avgRating: -1}}).limit(10).lean();
+        return res.send({sid: req.session.sid, courses: courses});
+    }
+}));
 
 router.get('/test/:courseCode', middleware.asyncMiddleware(async (req, res) => {
     let section = await Course.findOne({courseCode: 'ELTU3014'});
     console.log(section);
 }));
-
-
 
 // login
 router.post('/login', (req, res, next) => {
